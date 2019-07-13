bd1bac981931ce0d29223fd2392c5897659efbf1
kalpeshkumawat <kalpeshkumawat72@gmail.com>
2019-07-13 16:06:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/demo/youtubedemo/MainActivity.java
+++ app/src/main/java/com/demo/youtubedemo/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello Kalpesh Kumawat", Toast.LENGTH_SHORT).show();
 
 
     }
