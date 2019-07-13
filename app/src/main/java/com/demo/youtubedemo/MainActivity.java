6c24a32bdbf40b56fe2f91e516fbc1cc5a2c99e9
kalpeshkumawat <kalpeshkumawat65@gmail.com>
2019-07-13 16:02:24 +0530

    Initial commit

---
+++ app/src/main/java/com/demo/youtubedemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.demo.youtubedemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.flipkart.youtubeview.activity.YouTubeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String API_KEY = "AIzaSyBmUdBkpeF17M7qcZdXG-Juzj8okSu9fes";
+
+    private String VIDEO_ID = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
